3dad387be029dbd8691a87c6363284c636c07d9b
Juan <jc.ramos.contact@gmail.com>
2021-07-18 11:33:34 -0400

    Added padding to main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "./layout/Layout";
 import InputField from "./inputFields/InputField";
-import { Box, makeStyles } from "@material-ui/core";
+import { Box, Container, makeStyles } from "@material-ui/core";
 import NoteList from "./NoteList/NoteList";
 import { useServerContext } from "./context/ServerContext";
 import { Route, Switch } from "react-router-dom";
@@ -25,28 +25,30 @@ function App() {
 
   return (
     <Layout>
-      <Switch>
-        {/* Status */}
-        <Route path="/status/:status" exact>
-          <NoteList
-            notes={notes}
-            deteleteNote={deleteNote}
-            archiveNote={archiveNote}
-          />
-        </Route>
-        {/* Tags */}
-        <Route path="/:tag?">
-          <Box className={classes.root}>
-            <InputField />
-
+      <Container maxWidth="lg">
+        <Switch>
+          {/* Status */}
+          <Route path="/status/:status" exact>
             <NoteList
               notes={notes}
               deteleteNote={deleteNote}
               archiveNote={archiveNote}
             />
-          </Box>
-        </Route>
-      </Switch>
+          </Route>
+          {/* Tags */}
+          <Route path="/:tag?">
+            <Box className={classes.root}>
+              <InputField />
+
+              <NoteList
+                notes={notes}
+                deteleteNote={deleteNote}
+                archiveNote={archiveNote}
+              />
+            </Box>
+          </Route>
+        </Switch>
+      </Container>
     </Layout>
   );
 }
