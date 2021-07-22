81597e4cd01c8fecd949ae61e57ef270afa77597
Juan <jc.ramos.contact@gmail.com>
2021-07-21 21:27:35 -0400

    Improved the shadow box of note cards

--- src/NoteList/NoteCard.tsx
+++ src/NoteList/NoteCard.tsx
@@ -26,7 +26,7 @@ const useStyles = makeStyles((props) =>
       gap: "1rem",
       "&:hover": {
         boxShadow:
-          "2px 2px 5px 0px rgba(0,0,0,0.1), -1px -1px 5px 0px rgba(0,0,0,0.1)",
+          "0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)",
       },
       "&:hover #headerButton": {
         visibility: "visible",
